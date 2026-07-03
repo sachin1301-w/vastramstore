07a05c22106a2e88abd01247718c31e389656cca
SACHIN KUMAR <sk.39648215@gmail.com>
2026-07-04 00:28:08 +0530

    Update products.ts

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -145,6 +145,28 @@ export const products: Product[] = [
   "badge": "NEW"
 },
   {
+  "id": "1783105044149-4nsw66q",
+  "name": "Raincoat",
+  "description": "JS-201",
+  "price": 1289,
+  "originalPrice": 1500,
+  "image": "https://cdn.phototourl.com/free/2026-07-03-ac42903b-18ee-45ef-a52b-88ae1775575c.jpg",
+  "images": [
+    "https://cdn.phototourl.com/free/2026-07-03-ac42903b-18ee-45ef-a52b-88ae1775575c.jpg",
+    "https://cdn.phototourl.com/free/2026-07-03-97c06b78-2ae0-4c3f-938b-765df8cc26f9.jpg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "L",
+    "XL",
+    "XXL"
+  ],
+  "inStock": true,
+    "stock":20,
+  "featured": false,
+  "badge": "TRENDING"
+},
+  {
   "id": "1781593590486-cfvcc8y",
   "name": "Zeel Raincoat ",
   "description": "JS201 (Orange-blue )",
